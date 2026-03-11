faab5b2071c701e0ca25f21eaeb0fa2357804f97
jrentas13 <javy.rentas@gmail.com>
2026-03-11 00:53:31 -0600

    Created recipe endpoints for obtaining a specific recipe by id and deleting a recipe from the database.

--- routes/recipes/index.js
+++ routes/recipes/index.js
@@ -1,12 +1,16 @@
 import getRecipeRoute from './getRecipes.js';
+import getRecipeIdRoute from './getRecipeId.js';
 import postRecipeRoute from './postRecipes.js';
 import putRecipeRoute from './putRecipes.js';
+import deleteRecipeRoute from './deleteRecipe.js';
 
 //PREFIX: /recipes
 const recipeRoutes = async (fastify, opts) => {
     await fastify.register(getRecipeRoute);
+    await fastify.register(getRecipeIdRoute);
     await fastify.register(postRecipeRoute);
     await fastify.register(putRecipeRoute);
+    await fastify.register(deleteRecipeRoute);
 };
 
 export default recipeRoutes;
