672aba86deb61ff58fee27e4d67868f341ef5da6
jrentas13 <javy.rentas@gmail.com>
2026-03-11 11:03:50 -0600

    Finished creating all ingredients endpoints.

--- routes/ingredients/index.js
+++ routes/ingredients/index.js
@@ -1,12 +1,14 @@
 import getIngredientRoute from './getIngredients';
 import postIngredientRoute from './postIngredients';
 import putIngredientRoute from './putIngredients';
+import deleteIngredientRoute from './deleteIngredient';
 
 // PREFIX: /ingredients
 const ingredientRoutes = async(fastify, opts) => {
     await fastify.register(getIngredientRoute);
     await fastify.register(postIngredientRoute);
     await fastify.register(putIngredientRoute);
+    await fastify.register(deleteIngredientRoute);
 };
 
 export default ingredientRoutes;
