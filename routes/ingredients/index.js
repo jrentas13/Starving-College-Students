3fa128f716475fa69b354c814a8b806b3d00533c
jrentas13 <javy.rentas@gmail.com>
2026-03-18 10:29:30 -0600

    Fixing ingredients endpoints. Started on instructions endpoints.

--- routes/ingredients/index.js
+++ routes/ingredients/index.js
@@ -1,10 +1,10 @@
-import getIngredientRoute from './getIngredients';
-import postIngredientRoute from './postIngredients';
-import putIngredientRoute from './putIngredients';
-import deleteIngredientRoute from './deleteIngredient';
+import getIngredientRoute from './getIngredients.js';
+import postIngredientRoute from './postIngredients.js';
+import putIngredientRoute from './putIngredients.js';
+import deleteIngredientRoute from './deleteIngredient.js';
 
 // PREFIX: /ingredients
-const ingredientRoutes = async(fastify, opts) => {
+const ingredientRoutes = async (fastify, opts) => {
     await fastify.register(getIngredientRoute);
     await fastify.register(postIngredientRoute);
     await fastify.register(putIngredientRoute);
